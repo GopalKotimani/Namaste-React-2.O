df4b5005a002615f7f69bc82d902b6f0b97880b3
Gopal-19 <60785555+Gopal-19@users.noreply.github.com>
2025-04-26 12:01:23 +0530

    Secondary container with movie list  and now playing movies and use popular movies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,24 +3,18 @@ import Header from './Header'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import MainContainer from './MainContainer';
 import SecondaryContainer from './SecondaryContainer';
+import usePopularMovies from '../hooks/usePopularMovies';
 
 export const Browse = () => {
 
   useNowPlayingMovies();
+  usePopularMovies();
 
   return (
     <div>
       <Header />
-    {/*
-      MainContainer 
-        - Video background 
-        - Video title
-      Secondary Container 
-        - Movieslist * n
-        - Cards * n 
-    */}
-    <MainContainer />
-    <SecondaryContainer />
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   )
 }
---
+++ src/components/MovieCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { IMG_CDN_URL } from '../utils/constants'
+
+export const MovieCard = ({posterPath}) => {
+  return (
+    <div className='w-48 pr-4'>
+        <img 
+        alt="Movies card" 
+        src={IMG_CDN_URL+posterPath}
+        />
+
+    </div>
+  )
+}
